b18296387246ffa42a1959a0faa4f161e5f8c1e5
poojanaidu07 <poojashetty62362@gmail.com>
2020-07-13 16:36:47 +0530

    initial commit

---
+++ First/First/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Add");
+            Console.WriteLine("2.Substract");
+            Console.WriteLine("3.Multiply");
+            Console.WriteLine("4.Divide");
+            Console.Write("Enter Choice(1-4):");
+            int ch = Int32.Parse(Console.ReadLine());
+           
+            int a,b,c;
+            
+
+            switch (ch) 
+            {
+                case 1:
+                    Console.Write("Enter A:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter B:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = a + b;
+                    Console.WriteLine("Sum = {0}", c);
+                    break;
+                case 2:
+                    Console.Write("Enter A:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter B:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = a - b;
+                    Console.WriteLine("Difference = {0}", c);
+                    break;
+                case 3:
+                    Console.Write("Enter A:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter B:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = a * b;
+                    Console.WriteLine("Product = {0}", c);
+                    break;
+                case 4:
+                    Console.Write("Enter A:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter B:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = a / b;
+                    Console.WriteLine("Quotient = {0}", c);
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ Second/Second/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+             int x = 10;
+        int y = 5;
+
+        x = x + y; 
+        y = x - y; 
+        x = x - y; 
+        Console.WriteLine("After swapping: x = "
+                          + x + ", y = " + y); 
+        }
+    }
+}
